80e1d5bb029cd07bd790d8b15559ec709847fed7
saharshirzad2026@gmail.com <saharshirzad2026@gmail.com>
2026-04-30 21:43:59 -0700

    teacher and admin migrations

---
+++ database/migrations/2026_05_01_044008_create_teachers_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('teachers', function (Blueprint $table) {
+            $table->id();
+            $table->string("photo");
+            $table->string("degree");
+            $table->string("phone_number");
+            $table->text("biography");
+            $table->foreignId("des_id")->constrained("designation");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('teachers');
+    }
+};
