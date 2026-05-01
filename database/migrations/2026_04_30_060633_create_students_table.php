e4ae96acff2bd8bd3eaa93164cf587823d9e1a57
saharshirzad2026@gmail.com <saharshirzad2026@gmail.com>
2026-04-30 21:39:07 -0700

    user and student migration

---
+++ database/migrations/2026_04_30_060633_create_students_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string("photo");
+            $table->string("phone_number");
+            $table->integer("tazkira_number");
+            $table->date("dob");
+            $table->foreignId("user_id")->constrained("user");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
