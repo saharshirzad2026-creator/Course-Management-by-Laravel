0697b65bf280d37e59351f44acb1301f7473728c
saharshirzad2026@gmail.com <saharshirzad2026@gmail.com>
2026-05-07 22:05:52 -0700

    chanhes

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -2,6 +2,14 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
     <head>
         @include('partials.head')
+
+        <style>
+            [x-cloak] {
+                display: none !important;
+            }
+        </style>
+        @filamentStyles
+        @vite('resources/css/app.css')
     </head>
     <body class="min-h-screen bg-white dark:bg-zinc-800">
         <flux:sidebar sticky stashable class="border-r border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900">
@@ -128,5 +136,7 @@
         {{ $slot }}
 
         @fluxScripts
+        @filamentScripts
+        @vite('resources/js/app.js')
     </body>
 </html>
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,16 @@
-import {
-    defineConfig
-} from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite'
+
+import laravel from 'laravel-vite-plugin'
+
+import tailwindcss from '@tailwindcss/vite'
+
 
 export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: [`resources/views/**/*`],
+            refresh: true,
         }),
         tailwindcss(),
     ],
-    server: {
-        cors: true,
-    },
-});
+})
